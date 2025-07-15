03727650c5128ea79c890d5fa468746d686b6d03
Seung Hyun Tark <makepin2r@gmail.com>
2025-07-16 01:19:24 +0900

    update: TodoList, TodoItem 투두 props 정의

--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -1,13 +1,21 @@
+import { FC } from "react";
 import { Card } from "./ui/card";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
+import { Todo } from "@/types/todo-type";
+import { formatDate } from "date-fns";
 
-const TodoItem = () => {
+interface TodoItemProps {
+  todo: Todo;
+}
+
+const TodoItem: FC<TodoItemProps> = ({ todo }) => {
   return (
     <Card className="flex justify-start items-center flex-row gap-1 px-4">
-      투두 내용
+      {todo.title} / {todo.description}
+      <p>{formatDate(todo.createdAt, "yyyy-mm-dd")}</p>
       <Button variant="outline">
-        <Link to="/todo/1">수정</Link>
+        <Link to={`/todo/${todo.id}`}>수정</Link>
       </Button>
       <Button variant="outline">삭제</Button>
     </Card>
--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,7 +1,19 @@
+import { FC } from "react";
+import { Todo } from "../types/todo-type";
 import TodoItem from "./todo-item";
 
-const TodoList = () => {
-  return <TodoItem />;
+interface TodoListProps {
+  todos: Todo[];
+}
+
+const TodoList: FC<TodoListProps> = ({ todos }) => {
+  return (
+    <div>
+      {todos.map((item) => (
+        <TodoItem key={item.id} todo={item} />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
