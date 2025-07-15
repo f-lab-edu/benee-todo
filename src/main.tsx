fbffa7e036f0480de983026ab219d85d48d8181f
Seung Hyun Tark <makepin2r@gmail.com>
2025-07-16 00:31:56 +0900

    chore: 페이지 컴포넌트명 수정 (kebab-case)

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import HomePage from "./pages/HomePage";
-import TodoPage from "./pages/TodoPage";
+import HomePage from "./pages/home-page";
+import TodoPage from "./pages/todo-page";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
---
+++ src/pages/home-page.tsx
@@ -0,0 +1,12 @@
+import TodoList from "@/components/todo-list";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <TodoList />
+    </div>
+  );
+};
+
+export default HomePage;
