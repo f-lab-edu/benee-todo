b37610c9f6452b14a24cc4bbe4631c7417713248
Seung Hyun Tark <makepin2r@gmail.com>
2025-07-17 00:46:36 +0900

    update: TodoList 투두 생성 폼 추가

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
+import CreateTodoBox from "./create-todo-box";
 import TodoItem from "./todo-item";
 import useTodos from "@/hooks/useTodos";
 import { paginate } from "@/utils/paginate";
 
 const TodoList = () => {
   const [page, setPage] = useState<number>(1);
-  const { todos, toggleTodo, deleteTodo } = useTodos();
+  const { todos, createTodo, toggleTodo, deleteTodo } = useTodos();
 
   const paginatedTodos = paginate(page, PAGE_SIZE, todos ?? []);
 
   return (
     <div className="w-full max-w-200 mx-auto flex flex-col gap-2">
+      <CreateTodoBox onCreate={createTodo} />
       {paginatedTodos.map((item) => (
         <TodoItem
           key={item.id}
