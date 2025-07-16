a3f86a7d6d95d9830bde9940ca7627e71cd08e9b
Seung Hyun Tark <makepin2r@gmail.com>
2025-07-17 00:41:40 +0900

    feat: todo 생성 폼 컴포넌트 정의

---
+++ src/components/create-todo-box.tsx
@@ -0,0 +1,59 @@
+import React, { SyntheticEvent } from "react";
+import { Card } from "./ui/card";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { Label } from "./ui/label";
+import { Todo } from "@/types/todo-type";
+
+interface CreateTodoBoxProps {
+  onCreate: (value: Omit<Todo, "id" | "createdAt" | "completed">) => void;
+}
+
+const CreateTodoBox = ({ onCreate }: CreateTodoBoxProps) => {
+  const createTodo = (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const titleInput = form.elements.namedItem("title") as HTMLInputElement;
+    const descInput = form.elements.namedItem(
+      "description"
+    ) as HTMLInputElement;
+
+    if (e.target) {
+      const newTodo: Omit<Todo, "id" | "createdAt" | "completed"> = {
+        title: titleInput.value ?? "",
+        description: descInput.value ?? "",
+      };
+
+      onCreate(newTodo);
+
+      titleInput.value = "";
+      descInput.value = "";
+    } else {
+      console.error("event target is not exist");
+    }
+  };
+
+  return (
+    <Card className="px-4">
+      <form action="submit" onSubmit={createTodo}>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="title">제목</Label>
+          <Input id="title" placeholder="Todo 제목을 입력해주세요." />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="description">내용</Label>
+          <Textarea id="description" placeholder="Todo 설명을 입력해주세요." />
+        </div>
+        <div className="flex justify-end items-center">
+          <Button variant="secondary" type="submit">
+            추가하기
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default CreateTodoBox;
