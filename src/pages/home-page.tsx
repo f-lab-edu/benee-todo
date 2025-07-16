ee3e08c822cac5d3d9203b37eb7848c319c52155
Seung Hyun Tark <makepin2r@gmail.com>
2025-07-17 00:45:40 +0900

    update: TodoList props 전달 방식 변경

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,11 +1,10 @@
 import TodoList from "@/components/todo-list";
-import { todos } from "@/dummyTodos";
 
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
-      <TodoList todos={todos} />
+      <TodoList />
     </div>
   );
 };
