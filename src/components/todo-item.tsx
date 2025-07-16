84a33e6a0b7d88a3e63a7172e3df96c887e2e108
Seung Hyun Tark <makepin2r@gmail.com>
2025-07-16 20:03:22 +0900

    update: TodoItem 체크박스 추가

--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -4,6 +4,8 @@ import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
 import { Todo } from "@/types/todo-type";
 import { formatDate } from "date-fns";
+import { Checkbox } from "./ui/checkbox";
+import { Label } from "./ui/label";
 
 interface TodoItemProps {
   todo: Todo;
@@ -11,13 +13,21 @@ interface TodoItemProps {
 
 const TodoItem: FC<TodoItemProps> = ({ todo }) => {
   return (
-    <Card className="flex justify-start items-center flex-row gap-1 px-4">
-      {todo.title} / {todo.description}
-      <p>{formatDate(todo.createdAt, "yyyy-MM-dd hh:mm")}</p>
-      <Button variant="outline">
-        <Link to={`/todo/${todo.id}`}>수정</Link>
-      </Button>
-      <Button variant="outline">삭제</Button>
+    <Card className="flex justify-between items-center flex-row gap-1 px-4">
+      <div className="flex justify-start items-start gap-1">
+        <Checkbox id={todo.id} />
+        <div>
+          <Label htmlFor={todo.id}>{todo.title}</Label>
+          <p>{todo.description}</p>
+        </div>
+      </div>
+      <div className="flex justify-end items-center gap-1">
+        <p>{formatDate(todo.createdAt, "yyyy-MM-dd hh:mm")}</p>
+        <Button variant="outline">
+          <Link to={`/todo/${todo.id}`}>수정</Link>
+        </Button>
+        <Button variant="outline">삭제</Button>
+      </div>
     </Card>
   );
 };
