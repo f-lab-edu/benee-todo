bbd2c326220cc3c78509f42cd3a54b863fc6437d
Seung Hyun Tark <makepin2r@gmail.com>
2025-07-17 22:34:10 +0900

    style: HomePage 제목 정렬

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -3,7 +3,7 @@ import TodoList from "@/components/todo-list";
 const HomePage = () => {
   return (
     <div className="w-full max-w-400 mx-auto px-10 flex flex-col gap-4 py-20">
-      <h1>HomePage</h1>
+      <h1 className="text-center">HomePage</h1>
       <TodoList />
     </div>
   );
