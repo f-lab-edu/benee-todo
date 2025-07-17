a4e730292bc1df0ee520fe9c7899ff4b9a6539bc
Seung Hyun Tark <makepin2r@gmail.com>
2025-07-17 20:46:13 +0900

    style: HomePage 레이아웃 정리

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -3,7 +3,7 @@ import TodoList from "@/components/todo-list";
 
 const HomePage = () => {
   return (
-    <div className="w-full max-w-400 mx-auto px-10 flex flex-col gap-4">
+    <div className="w-full max-w-400 mx-auto px-10 flex flex-col gap-4 py-20">
       <h1>HomePage</h1>
       <CreateTodoBox />
       <TodoList />
